14616a0545fada8b763e20f97a2626cc33094d9b
AJ Amelio <aj.amelio90@gmail.com>
2019-07-24 12:14:06 -0400

    Delete addcomma.pipe.spec.ts

--- front-end/src/app/pipes/addcomma.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddcommaPipe } from './addcomma.pipe';
-
-describe('AddcommaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AddcommaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
